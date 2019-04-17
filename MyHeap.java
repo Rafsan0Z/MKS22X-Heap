0b705b659a17cd5cd6f79bf8498ef03479da4795
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-04-17 16:52:02 -0400

    all methods added!

--- MyHeap.java
+++ MyHeap.java
@@ -1,3 +1,24 @@
 pubic class MyHeap{
 
+  private static void pushDown(int[]data,int size,int index){
+
+  }
+
+  private static void pushUp(int[]data,int index){
+
+
+  }
+
+  public static void heapify(int[]){
+
+
+  }
+
+
+  public static void heapsort(int[]){
+
+    
+  }
+
+
 }
